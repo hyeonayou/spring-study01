a8766b2f176a4aa53150b2af744f0303d057e884
hyeonayou <youhyeona99@gmail.com>
2023-04-06 07:20:09 +0900

    add Member API

---
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -0,0 +1,23 @@
+package hello.hellospring.domain;
+
+public class Member {
+
+    private Long id;
+    private String naem;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNaem() {
+        return naem;
+    }
+
+    public void setNaem(String naem) {
+        this.naem = naem;
+    }
+}
